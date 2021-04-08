a1d3dc9e34c07818ad3087c031d8219bff520e0e
minah <minah8800@gmail.com>
2021-04-08 14:02:59 +0900

    second commit

--- app_cookie.js
+++ app_cookie.js
@@ -27,7 +27,42 @@ app.get('/count', function(req, res) {
     count += 1;
     res.cookie('count', count);
     res.send('count : '+ count);
-})
+});
+/*
+cart = {
+    1:2,
+    2:1
+}
+*/
+app.get('/cart/:id', function(req, res) {
+    var id = req.params.id;
+    if(req.cookies.cart) { // 다음 실행 => 값이 있으면 넣기
+        var cart = req.cookies.cart;
+    } else {
+        var cart = {};
+    }
+    if(!cart[id]) {
+        cart[id] = 0;
+    }
+    cart[id] = parseInt(cart[id])+1;
+    res.cookie('cart', cart); // cookies.cart값 넣기
+    res.redidrect('/cart');
+});
+app.get('/cart', function(req, res) {
+    var cart = req.cookies.cart;
+    if(!cart) {
+        res.send('Empty');
+    } else {
+        var output = '';
+        for(var id in cart) { // cookies.cart{}
+            output += `<li>${products[id].title} (${cart[id]})</li>`;
+        }
+    }
+    res.send(`
+        <h1>Cart</h1>
+        <ul>${output}</ul>
+        <a href="/products">Products List</a>`);
+});
 app.listen(3003, function(){
     console.log('Connected 3003 port');
 });
